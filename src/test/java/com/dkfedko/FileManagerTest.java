25a3e80b142224c475cae6bf9d9c42200f71810e
DKfedko <dkfedko@yahoo.com>
2025-11-06 13:54:41 -0500

    add test creating directories

--- src/test/java/com/dkfedko/FileManagerTest.java
+++ src/test/java/com/dkfedko/FileManagerTest.java
@@ -1,7 +1,8 @@
 package com.dkfedko;
 import org.junit.jupiter.api.Test;
-
 import java.io.File;
+import java.io.IOException;
+import java.lang.reflect.Array;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -62,15 +63,101 @@ public class FileManagerTest {
     }
 
     @Test
-    void shouldCreateDirectoryCheckAndDelete(){
+    void shouldCreateDirectoryCheckAndDelete() {
 
-        File file = new File("dir1");
+        try {
+            File dir1 = new File("src/dir1");
+            File dir1_1 = new File("src/dir1/dir1_1");
+            File dir1_2 = new File("src/dir1/dir1_2");
+            File dir2 = new File("src/dir2");
+            File dir3 = new File("src/dir3");
 
-        boolean create = file.mkdirs();
-        if (!file.exists()){
-            throw new RuntimeException("could not create directory ");
+            File file0 = new File("src/file0.txt");
+            File file1 = new File("src/dir1/file1.txt");
+            File file1_1 = new File("src/dir1/dir1_1/file1_1.txt");
+            File file1_2 = new File("src/dir1/dir1_2/file1_2.txt");
+            File file2 = new File("src/dir2/file2.txt");
+
+            if (dir1.exists()){
+                System.out.println("directory " + dir1 + " already exists");
+            }else{
+                dir1.mkdirs();
+                System.out.println("directory " + dir1 + " created");
+            }
+
+            if (dir1_1.exists()){
+                System.out.println("directory " + dir1_1 + " already exists");
+            }else {
+                dir1_1.mkdirs();
+                System.out.println("directory " + dir1_1 + " created");
+            }
+
+            if (dir1_2.exists()){
+                System.out.println("directory " + dir1_2 + " already exists");
+            }else {
+                dir1_2.mkdirs();
+                System.out.println("directory " + dir1_2 + " created");
+            }
+
+            if (dir2.exists()){
+                System.out.println("directory " + dir2 + " already exists");
+            }else{
+                dir2.mkdirs();
+                System.out.println("directory " + dir2 + " created");
+            }
+            if (dir3.exists()){
+                System.out.println("directory " + dir3 + " already exists");
+            }else{
+                dir3.mkdirs();
+                System.out.println("directory " + dir3 + " created");
+            }
+
+            if (file0.exists()){
+                System.out.println("file " + file0 + " created");
+            }else{
+                file0.createNewFile();
+                System.out.println("file " + file0 + " already exists");
+            }
+
+            if (file1.exists()){
+                System.out.println("file " + file1 + " created");
+            }else{
+                file1.createNewFile();
+                System.out.println("file " + file1 + " already exists");
+            }
+
+            if (file1_1.exists()){
+                System.out.println("file " + file1_1 + " created");
+            }else{
+                file1_1.createNewFile();
+                System.out.println("file " + file1_1 + " already exists");
+            }
+
+            if (file1_2.exists()){
+                System.out.println("file " + file1_2 + " created");
+            }else{
+                file1_2.createNewFile();
+                System.out.println("file " + file1_2 + " already exists");
+            }
+            if (file2.exists()){
+                System.out.println("file " + file2 + " created");
+            }else{
+                file2.createNewFile();
+                System.out.println("file " + file2 + " already exists");
+            }
+
+
+            if (file0.delete()){
+                System.out.println("file " + file0 + " deleted");
+            }else{
+                file0.deleteOnExit();
+                System.out.println("file " + file0 + " not deleted");
+            }
         }
-
+        catch (IOException e) {
+            throw new RuntimeException("can't create directory", e);
+        }
     }
-
 }
+
+
